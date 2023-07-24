69de25309ed9947fae775d9c17a23b31dafc7ad6
abdel-ke <kemmal1337@gmail.com>
2023-07-24 20:22:03 +0100

    fix errors

--- main.c
+++ main.c
@@ -10,11 +10,42 @@
 int main(void)
 {
 	int len1 = 0, len2 = 0;
+	len1 = printf("|%c|", '0');
+	len2 = _printf("|%c|", '0');
+	printf("|%d|\t|%d|\n", len1, len2);
+	
+	len1 = printf(" |%c| ", '0');
+	len2 = _printf(" |%c| ", '0');
+	printf("|%d|\t|%d|\n", len1, len2);
 
-	len1  = _printf("");
-	len2 = printf("");
 
-	printf("len1: |%d|\n", len1);
-	printf("len2: |%d|\n", len2);
+	len1 = printf(" |%c|", '0' - 256);
+	len2 = _printf(" |%c|", '0' - 256);
+	printf("|%d|\t|%d|\n", len1, len2);
+
+	len1 = printf("|%c| ", '0' + 256);
+	len2 = _printf("|%c| ", '0' + 256);
+	printf("|%d|\t|%d|\n", len1, len2);
+
+	len1 = printf(" |%c| |%c| |%c| ", '0', 0, '1');
+	len2 = _printf(" |%c| |%c| |%c| ", '0', 0, '1');
+	printf("|%d|\t|%d|\n", len1, len2);
+
+	len1 = printf(" |%c| |%c| |%c| ", ' ', ' ', ' ');
+	len2 = _printf(" |%c| |%c| |%c| ", ' ', ' ', ' ');
+	printf("|%d|\t|%d|\n", len1, len2);
+
+	len1 = printf(" |%c| |%c| |%c| ", '1', '2', '3');
+	len2 = _printf(" |%c| |%c| |%c| ", '1', '2', '3');
+	printf("|%d|\t|%d|\n", len1, len2);
+
+	len1 = printf(" |%c| |%c| |%c| ", '2', '1', 0);
+	len2 = _printf(" |%c| |%c| |%c| ", '2', '1', 0);
+	printf("|%d|\t|%d|\n", len1, len2);
+
+	len1 = printf(" |%c| |%c| |%c| ", 0, '1', '2');
+	len2 = _printf(" |%c| |%c| |%c| ", 0, '1', '2');
+	printf("|%d|\t|%d|\n", len1, len2);
+
 	return (0);
 }
